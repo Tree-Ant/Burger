dbe018b5d25c88763dee033be7c24b71167b0d7c
Tree Ant <stokes.finance@gmail.com>
2019-12-17 19:06:04 -0600

    burger.js complete

--- models/burger.js
+++ models/burger.js
@@ -1,21 +1,24 @@
 var orm = require("../config/orm.js");
 // ============IMPORT============
 
+var burger = {
+    selectAll: function(cb) {
+      orm.selectAll("burgers", function(res) {
+        cb(res);
+      });
+    },
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    insertOne: function(cols, vals, cb) {
+      orm.insertOne("burgers", cols, vals, function(res) {
+        cb(res);
+      });
+    },
+    updateOne: function(objColVals, condition, cb) {
+      orm.updateOne("burgers", objColVals, condition, function(res) {
+        cb(res);
+      });
+    }
+  };
 
 // ============EXPORT============
 module.exports = burger;
