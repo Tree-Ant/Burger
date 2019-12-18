0b271e5137b7eb7e34b2581cae1e62df20666af1
Tree Ant <stokes.finance@gmail.com>
2019-12-18 11:52:15 -0600

    server

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,19 @@
 var mysql = require("mysql");
 
+// var connection = mysql.createConnection({
+//   host: "sp6xl8zoyvbumaa2.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+//   port: 3306,
+//   user: "yef6gporixvab0yi",
+//   password: "fbp08qic7dfxxxtp",
+//   database: "wfeebcaocu2c5ur4"
+// });
+
 var connection = mysql.createConnection({
-  host: "sp6xl8zoyvbumaa2.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-  port: 3306,
-  user: "yef6gporixvab0yi",
-  password: "fbp08qic7dfxxxtp",
-  database: "wfeebcaocu2c5ur4"
+  host: "localhost",
+  port: 3307,
+  user: "root",
+  password: "",
+  database: "burgers_db"
 });
 
 connection.connect(function(err) {
