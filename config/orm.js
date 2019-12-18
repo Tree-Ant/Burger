2d83f69f2ee7c59413e3442e3715009989d118e8
Tree Ant <stokes.finance@gmail.com>
2019-12-17 18:56:38 -0600

    orm complete

--- config/orm.js
+++ config/orm.js
@@ -1,19 +1,65 @@
 var connection = require("../config/connection.js");
 // ============IMPORT============
+function printQuestionMarks(num) {
+    var arr = [];
+  
+    for (var i = 0; i < num; i++) {
+      arr.push("?");
+    }
+  
+    return arr.toString();
+  }
 
+function objToSql(ob) {
+    var arr = [];
 
-selectAll()
+    for (var key in ob) {
+        var value = ob[key];
+        if (Object.hasOwnProperty.call(ob, key)) {
+            if (typeof value === "string" && value.indexOf(" ") >= 0) {
+                value = "'" + value + "'";
+            }
+            arr.push(key + "=" + value);
+        }
+    }
 
-insertOne()
+    // translate array of strings to a single comma-separated string
+    return arr.toString();
+}
 
-updateOne()
+var orm = {
+    selectAll: function (tableInput, cb) {
+        var queryString = "SELECT * FROM burgers";
+        connection.query(queryString, function (err, result) {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+    },
 
+    insertOne: function (table, cols, vals, cb) {
+        var queryString = `INSERT INTO ${table} ("${cols}") VALUES ("${printQuestionMarks(vals.length)}")`;
+        console.log(queryString);
+        connection.query(queryString, (err, res) => {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+    },
 
+    updateOne: function (table, objColVals, condition, cb) {
+        const queryString = `UPDATE ${table} SET ${objToSql(objColVals)} WHERE ${condition}`;
+        connection.query(queryString, (err, res) => {
+            if (err) {
+                throw err;           
+            }
 
-
-
-
-
+            cb(result);
+    });
+    }
+};
 
 // ============EXPORT============
 module.exports = orm;
