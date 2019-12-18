0ea125ce072d79f093a8a7e77cb7b2ca569f25fa
Tree Ant <stokes.finance@gmail.com>
2019-12-17 23:18:13 -0600

    project functionality  complete

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,47 +1,48 @@
-$(function() {
-    $(".change-devoured").on("click", function(event) {
-      var id = $(this).data("id");
-      var newDevored = $(this).data("newdevoured");
-        var newDevouredState = {
-        devoured: newDevoured
-      };
+$(function () {
+    $(".change-devour").on("click", function (event) {
+        var id = $(this).data("id");
+        var newDevour = $(this).data("newdevour");
+        
+        var newDevourState = {
+            devoured: newDevour
+        };
 
-      $.ajax("/api/burgers/" + id, {
-        type: "PUT",
-        data: newDevouredState
-      }).then(
-        function() {          
-          location.reload();
-        }
-      );
+        $.ajax("/api/burgers/" + id, {
+            type: "PUT",
+            data: newDevourState
+        }).then(
+            function () {
+                location.reload();
+            }
+        );
     });
-  
-    $(".create-form").on("submit", function(event) {
-      event.preventDefault();  
-      var newBurger = {
-        name: $("#burger").val().trim(),
-        devoured: $("[name=devoured]:checked").val().trim()
-      };
-  
-      $.ajax("/api/burgers", {
-        type: "POST",
-        data: newBurger
-      }).then(
-        function() {
-          location.reload();
-        }
-      );
+
+    $(".create-form").on("submit", function (event) {
+        event.preventDefault();
+        var newBurger = {
+            burger_name: $("#bu").val().trim(),
+            devoured: $("[name=devoured]:checked").val().trim()
+        };
+
+        $.ajax("/api/burgers", {
+            type: "POST",
+            data: newBurger
+        }).then(
+            function () {
+                location.reload();
+            }
+        );
     });
 });
-/* 
+/*
     $(".delete-burger").on("click", function(event) {
       var id = $(this).data("id");
-  
+
       // Send the DELETE request.
       $.ajax("/api/burgers/" + id, {
         type: "DELETE"
       }).then(
-        function() {          
+        function() {
           location.reload();
         }
       );
